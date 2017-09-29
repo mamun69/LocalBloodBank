d103294a9a71606fb02af5619091e4789deed9d0
mamun69 <mamun007j69@gmail.com>
2017-09-29 19:08:57 +0600

    LocalBloodBank project is added

---
+++ LocalBloodBank/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LocalBloodBank.MvcApplication" Language="C#" %>
---
+++ LocalBloodBank/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LocalBloodBank.Startup))]
+namespace LocalBloodBank
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

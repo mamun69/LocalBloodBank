92a690a06f3a9438d7e70bc0fcfecbf9233b4c0b
mamun69 <mamun007j69@gmail.com>
2017-09-29 19:40:06 +0600

    BloodDoner Model is created

---
+++ LocalBloodBank/DomainModels/BloodDoner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LocalBloodBank.DomainModels
+{
+    public class BloodDoner
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        public string bloodGroup { get; set; }
+        public string permanentAddress { get; set; }
+        public string presentAddress { get; set; }
+        public string phoneNumber { get; set; }
+        public string occupation { get; set; }
+        public string region { get; set; }
+
+        public DateTime lastDonationDate { get; set; }
+    }
+}

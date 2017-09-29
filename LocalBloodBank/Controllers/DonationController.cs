9a7f511a1dd52a2a9197a5b1ce2d7ca78ae9fdc3
mamun69 <mamun007j69@gmail.com>
2017-09-29 20:10:03 +0600

    Donation controller is created with DonerList, CreateDoner, UpdateDoner and DeleteDoner actions. Corsponding view pages for controller action is also created

---
+++ LocalBloodBank/Controllers/DonationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LocalBloodBank.Controllers
+{
+    public class DonationController : Controller
+    {
+        //
+        // GET: /Donation/
+        public ActionResult DonerList()
+        {
+            return View();
+        }
+
+        public ActionResult CreateDoner()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateDoner()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteDoner()
+        {
+            return View();
+        }
+	}
+}
